03891be46259a8df0dae75319b3c6e4861e9f183
b9zhengaoxing <1164834201@qq.com>
2016-01-21 22:26:14 +0800

    GCD2
    延时执行

--- GCD /GCDDemo/GCDDemo/ViewController.m
+++ GCD /GCDDemo/GCDDemo/ViewController.m
@@ -20,16 +20,33 @@
 //    [self serialQueue];
 //    [self initConCurrent];
     
-    [GCDQueue executeInGlobalQueue:^{
-        // 子线程——业务逻辑
-        // 如果在这里写了UI，因为是串行，会卡界面
-        
-        
-        [GCDQueue executeInMainQueue:^{
-            // 主线程——更新UI
-        }];
-    }];
+//    [GCDQueue executeInGlobalQueue:^{
+//        // 子线程——业务逻辑
+//        // 如果在这里写了UI，因为是串行，会卡界面
+//        
+//        
+//        [GCDQueue executeInMainQueue:^{
+//            // 主线程——更新UI
+//        }];
+//    }];
+    NSLog(@"主线程执行");
+    [self performSelector:@selector(threadEvent:)
+               withObject:self
+               afterDelay:2.0f];
+    //Cancels perform requests previously registered with the performSelector:withObject:afterDelay: instance method.
+    //All perform requests having the same target aTarget are canceled. This method removes perform requests only in the current run loop, not all run loops.
+
+    [NSObject cancelPreviousPerformRequestsWithTarget:self];
     
+    [GCDQueue executeInMainQueue:^{
+        NSLog(@"GCD 执行");
+    } afterDelaySecs:2.0f];
+    
+}
+
+- (void)threadEvent:(id)sender
+{
+    NSLog(@"Thead 事件执行");
 }
 
 //串行队列
